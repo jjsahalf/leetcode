63563d1fbfdd5a81f4186c9c7ed6a2eab6d3a0aa
Yufei Jiang <jjsahalf@gmail.com>
2015-09-28 01:25:25 -0400

    mySqrt

---
+++ src/leetcode/MySqrtSolution.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * Created by yufeijiang on 9/28/15.
+ */
+public class MySqrtSolution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+
+
+        int lowerRoot=1;
+        int upperRoot=x-1;
+        int middleRoot=(lowerRoot+upperRoot)/2;
+        while(upperRoot-lowerRoot>1){
+            if(middleRoot*middleRoot==x){
+                return middleRoot;
+            }
+            long middleInLong=middleRoot;
+            long middleSquare=middleInLong*middleInLong;
+            if( middleSquare>x){
+                upperRoot=middleRoot;
+                middleRoot=(upperRoot+lowerRoot)/2;
+            }else{
+                lowerRoot=middleRoot;
+                middleRoot=(upperRoot+lowerRoot)/2;
+            }
+        }
+
+        return lowerRoot;
+    }
+}
