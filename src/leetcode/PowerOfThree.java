7db34521abbe32609a4fc89ca8d2bfa53660360a
Yufei Jiang <jjsahalf@gmail.com>
2016-02-17 13:48:00 -0500

    comment a little

--- src/leetcode/PowerOfThree.java
+++ src/leetcode/PowerOfThree.java
@@ -4,6 +4,11 @@ package leetcode;
  * Created by yufeijiang on 2/17/16.
  */
 public class PowerOfThree {
+
+    /*
+    This solution is Bravo!!!!
+     */
+
     public boolean isPowerOfThree(int n) {
         if(n<=0){
             return false;
