94a83e427ab58c0c88bb59af80b73ec91e1ecacd
Yufei Jiang <jjsahalf@gmail.com>
2016-02-17 13:44:19 -0500

    power of three

---
+++ src/leetcode/PowerOfThree.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * Created by yufeijiang on 2/17/16.
+ */
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }else{
+            Integer integer=n;
+            return integer.toString(n,3).matches("10*");
+        }
+    }
+}
