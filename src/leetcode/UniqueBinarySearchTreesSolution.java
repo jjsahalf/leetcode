03bc9e1340536bb4f82d3c604b84ebab1dfd8775
Yufei Jiang <jjsahalf@gmail.com>
2016-06-23 22:31:24 -0700

    Unique Binary Search Trees Solution

---
+++ src/leetcode/UniqueBinarySearchTreesSolution.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * Created by yufeijiang on 6/23/16.
+ */
+public class UniqueBinarySearchTreesSolution {
+    public int numTrees(int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 1;
+        }
+
+        int[] dp= new int [n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n; i++){
+            int sum=0;
+            for(int j=0;j<=i-1;j++){
+                sum+=dp[j]*dp[i-1-j];
+            }
+            dp[i]=sum;
+        }
+        return dp[n];
+    }
+}
