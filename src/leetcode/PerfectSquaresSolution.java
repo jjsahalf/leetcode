43cc498963e2019ae80db160f9d069931426084b
Yufei Jiang <jjsahalf@gmail.com>
2016-09-06 20:08:50 -0400

    perfect square

--- src/leetcode/PerfectSquaresSolution.java
+++ src/leetcode/PerfectSquaresSolution.java
@@ -4,6 +4,28 @@ package leetcode;
  * Created by yufeijiang on 9/6/16.
  */
 public class PerfectSquaresSolution {
+
+    /*
+    public int numSquares(int n) {
+    int max = (int) Math.sqrt(n);
+
+    int[] dp = new int[n+1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=max; j++){
+            if(i==j*j){
+                dp[i]=1;
+            }else if(i>j*j){
+                dp[i]=Math.min(dp[i], dp[i-j*j] + 1);
+            }
+        }
+    }
+
+    return dp[n];
+}
+     */
+
     public int numSquares(int n) {
         int[] dp=new int[n+1];
         int root=1;
